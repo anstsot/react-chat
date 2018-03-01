395c9f9cfb93e5a3002b699d6e08eccc3d7adc2c
Anastasiia <an.otrubchak@gmail.com>
2018-03-01 15:32:22 +0200

    Add Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,16 @@
 import React from 'react';
-import 'typeface-roboto';
-import Sidebar from './Sidebar';
-import ChatHeader from './ChatHeader';
-import Chat from './Chat';
-import { withStyles } from 'material-ui/styles';
-import { chats, messages } from '../mock-data';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import ChatPage from './ChatPage';
+import WelcomePage from './WelcomePage';
 
-const styles = theme => ({
-  root: {
-    position: 'relative',
-    display: 'flex',
-    width: '100%',
-    height: '100%',
-    backgroundColor: theme.palette.background.default,
-  },
-});
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/chat" component={ChatPage} />
+      <Redirect to="/" />
+    </Switch>
+  </Router>
+);
 
-const App = ({classes}) => {
-  return (
-    <div className={classes.root}>
-      <ChatHeader />
-      <Sidebar chats={chats} />
-      <Chat messages={messages} />
-    </div>
-  );
-};
-
-export default withStyles(styles)(App);
+export default App;
