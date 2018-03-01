2355835ab573624b13477deec5891d2dda34d13c
Anastasiia <an.otrubchak@gmail.com>
2018-03-01 21:19:01 +0200

    Add Forms

---
+++ src/components/LoginForm.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+import { withStyles } from 'material-ui';
+
+const styles = theme => ({
+  container: {
+    padding: theme.spacing.unit * 2,
+  },
+  button: {
+    marginTop: theme.spacing.unit * 2,
+  }
+});
+
+class LoginForm extends React.Component {
+  state = {
+    username: '',
+    password: '',
+  };
+
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
+  render () {
+    const { classes } = this.props;
+
+    return (
+      <form className={classes.container} noValidate>
+        <TextField
+          id="username"
+          label="Username"
+          placeholder="Type your username..."
+          type="text"
+          className={classes.textField}
+          value={this.state.username}
+          onChange={this.handleChange('username')}
+          margin="normal"
+          fullWidth
+          required
+        />
+        <TextField
+          id="password"
+          label="Password"
+          placeholder="Type your password..."
+          type="password"
+          className={classes.textField}
+          value={this.state.password}
+          onChange={this.handleChange('password')}
+          margin="normal"
+          fullWidth
+          required
+        />
+        <Button fullWidth variant="raised" type="submit" color="primary" className={classes.button}>Login</Button>
+      </form>
+    );
+  };
+}
+
+export default withStyles(styles)(LoginForm);
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,9 +1,75 @@
 import React from 'react';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Paper from 'material-ui/Paper';
+import Tabs, { Tab } from 'material-ui/Tabs';
+import { withStyles } from 'material-ui';
+import LoginForm from './LoginForm';
+import SignUpForm from './SignUpForm';
 
-const WelcomePage = () => {
-  return (
-    <h1>Welcome!</h1>
-  );
-};
+const styles = theme => ({
+  root: {
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    backgroundColor: theme.palette.background.default,
+  },
+  formWrapper: {
+    width: '100%',
+    height: '100%',
+    minHeight: `calc(100vh - 64px)`,
+    marginTop: '64px',
+    display: 'block',
+    backgroundColor: '#d6e9f7',
+  },
+  divPaper: {
+    width: 400,
+    marginTop: theme.spacing.unit * 5,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    backgroundColor: theme.palette.background.paper,
+  }
+});
 
-export default WelcomePage;
+class WelcomePage extends React.Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render () {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <Typography variant="title" color="inherit" noWrap>
+              Welcome Page React Chat
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <div className={ classes.formWrapper }>
+          <Paper className={ classes.divPaper } elevation={6}>
+            <AppBar position="static">
+              <Tabs value={value} onChange={this.handleChange} fullWidth>
+                <Tab label="Login" />
+                <Tab label="Sign Up" />
+              </Tabs>
+            </AppBar>
+            {value === 0 && <LoginForm/>}
+            {value === 1 && <SignUpForm/>}
+          </Paper>
+        </div>
+      </div>
+    );
+  };
+}
+
+export default withStyles(styles)(WelcomePage);
